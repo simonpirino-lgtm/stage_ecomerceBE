fb532a5e47fb4862a289cb017b1bc6c46524b809
simonpirino-lgtm <simonpirino@gmail.com>
2026-04-09 10:13:22 +0200

    aggiunto il server

---
+++ server.js
@@ -0,0 +1,30 @@
+// index.js  server.js
+require('dotenv').config();
+const app = require('./app');
+const { sequelize } = require('./src/models');
+
+
+const PORT = process.env.PORT || 3000;
+
+const start = async () => 
+{
+  try 
+  {
+    await sequelize.authenticate();
+    //await sequelize.sync();// ignorato: le migrazioni CLI gestiscono lo schema. sync() in produzione rischia di alterare tabelle esistenti.
+
+    console.log('Connessione al database StageSS26IOT_BE_Analytics API riuscita');
+
+    app.listen(PORT, () => 
+    {
+      console.log(`Server avviato su http://localhost:${PORT}`,`\nDocumentazione su http://localhost:${PORT}/api-docs`);
+    });
+  } 
+  catch (error) 
+  {
+    console.error('Errore connessione database:', error);
+    process.exit(1);
+  }
+};
+
+start();
