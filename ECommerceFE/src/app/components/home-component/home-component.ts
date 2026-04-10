3760e461bdfc4a2df593545cb0eaf734b11deba3
andre-a520 <andreafilippocontu5758@gmail.com>
2026-04-10 15:28:58 +0200

    ss

--- ECommerceFE/src/app/components/home-component/home-component.ts
+++ ECommerceFE/src/app/components/home-component/home-component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit {
   giochiModel: GiochiModel[] = [];
   searchTerm: string = ''; 
 
+  
   private giochiService = inject(GiochiService);
   private cdr = inject(ChangeDetectorRef);
 
