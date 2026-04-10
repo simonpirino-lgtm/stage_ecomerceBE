36c3549f664f09b7dbeb4af9a449f89122a93f26
simonpirino-lgtm <simonpirino@gmail.com>
2026-04-10 11:54:12 +0200

    abbiamo collegato angular

---
+++ ECommerceFE/src/app/models/giochi-model.ts
@@ -0,0 +1,21 @@
+export class GiochiModel 
+{
+    
+    id!: number;
+    titolo!: string;
+    prezzo!: string;
+    datarilascio!: string;
+    sviluppatore!: string;
+    image_url!: string;
+    descrizione!: string;
+}
+ /*"giochi": [
+    {
+      "id": 1,
+      "titolo": "The Witcher 3: Wild Hunt",
+      "prezzo": "29.99",
+      "datarilascio": "2015-05-19",
+      "sviluppatore": "CD Projekt Red",
+      "image_url": "https://cdn.cloudflare.steamstatic.com/steam/apps/292030/header.jpg",
+      "descrizione": "Open world action RPG set in a dark fantasy universe."
+    } */
