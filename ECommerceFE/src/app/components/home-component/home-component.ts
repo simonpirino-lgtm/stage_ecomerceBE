aee2926c7bc65de6c81cb0f53e6a437ec53a20a8
DavideRIFTS <daviderossini@yahoo.com>
2026-04-10 12:42:13 +0200

    Ferse

--- ECommerceFE/src/app/components/home-component/home-component.ts
+++ ECommerceFE/src/app/components/home-component/home-component.ts
@@ -1,25 +1,29 @@
 import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { GiochiModel } from '../../models/giochi-model';
 import { GiochiService } from '../../services/giochi-service';
+import { CommonModule } from '@angular/common';
 
 @Component
 ({
   selector: 'app-home-component',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
-export class HomeComponent 
+export class HomeComponent
 {
    giochiModel : GiochiModel[] = [];
    private giochiService = inject(GiochiService);
    private cdr = inject(ChangeDetectorRef);
     ngOnInit()
     {
-      this.giochiService.getGiochi().subscribe((data) => 
-      {
-        this.giochiModel = data;
-        this.cdr.detectChanges();
+     this.giochiService.getGiochi().subscribe({
+        next: (data) => {
+          console.log('Dati ricevuti:', data);
+          this.giochiModel = data;
+          this.cdr.detectChanges();
+        },
+        error: (err) => console.error('Errore nel caricamento:', err)
       });
     }
 }
