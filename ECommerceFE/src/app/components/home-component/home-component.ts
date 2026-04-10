771ba2f1580e49eb54ed997d29dfd7bf25a65406
simonpirino-lgtm <simonpirino@gmail.com>
2026-04-10 16:32:38 +0200

    ok

--- ECommerceFE/src/app/components/home-component/home-component.ts
+++ ECommerceFE/src/app/components/home-component/home-component.ts
@@ -1,25 +1,64 @@
-import { ChangeDetectorRef, Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { GiochiModel } from '../../models/giochi-model';
 import { GiochiService } from '../../services/giochi-service';
 
-@Component
-({
+@Component({
   selector: 'app-home-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './home-component.html',
   styleUrl: './home-component.css',
 })
-export class HomeComponent 
-{
-   giochiModel : GiochiModel[] = [];
-   private giochiService = inject(GiochiService);
-   private cdr = inject(ChangeDetectorRef);
-    ngOnInit()
-    {
-      this.giochiService.getGiochi().subscribe((data) => 
-      {
+export class HomeComponent implements OnInit {
+  giochiModel: GiochiModel[] = [];
+  searchTerm: string = ''; 
+  transforms: { [id: number]: string } = {};
+
+  private giochiService = inject(GiochiService);
+  private cdr = inject(ChangeDetectorRef);
+
+  get giochiFiltrati() {
+    return this.giochiModel.filter(gioco =>
+      gioco.titolo.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  ngOnInit() {
+    this.giochiService.getGiochi().subscribe({
+      next: (data: GiochiModel[]) => {
         this.giochiModel = data;
         this.cdr.detectChanges();
-      });
-    }
+      },
+      error: (err: any) => console.error("Errore nel caricamento dati:", err)
+    });
+  }
+
+  aggiungiAlCarrello(gioco: GiochiModel) {
+    alert(`Aggiunto al carrello: ${gioco.titolo}`);
+  }
+
+  onMouseEnter(event: MouseEvent, gioco: GiochiModel) {
+    this.transforms[gioco.id] = 'scale(1.2)';
+  }
+
+  onMouseMove(event: MouseEvent, gioco: GiochiModel) {
+    const rect = (event.target as HTMLElement).getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -15;
+    const rotateY = ((x - centerX) / centerX) * 15;
+
+    this.transforms[gioco.id] =
+      `scale(1.2) perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+  }
+
+  onMouseLeave(gioco: GiochiModel) {
+    delete this.transforms[gioco.id];
+  }
 }
