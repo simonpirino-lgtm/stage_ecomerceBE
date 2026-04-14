4297b44d28a0edfd9d2be022b00baaa8806332b8
andre-a520 <andreafilippocontu5758@gmail.com>
2026-04-14 16:46:41 +0200

    redirect

--- ECommerceFE/src/app/components/home-component/home-component.ts
+++ ECommerceFE/src/app/components/home-component/home-component.ts
@@ -36,6 +36,9 @@ export class HomeComponent implements OnInit {
     const storedUser = localStorage.getItem('user');
     if (storedUser) {
       this.user = JSON.parse(storedUser);
+    } else {
+      this.router.navigate(['/']); // redirect to login
+      return;
     }
 
     this.giochiService.getGiochi().subscribe({
@@ -66,7 +69,7 @@ export class HomeComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('user');
-    this.router.navigate(['/auth/login']);
+    this.router.navigate(['/']);
   }
 
   aggiungiAlCarrello(gioco: GiochiModel) {
