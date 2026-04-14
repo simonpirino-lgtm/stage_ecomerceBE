9a4fdf247e0f02ddb445c25e64d0f252f4d8bcd9
simonpirino-lgtm <simonpirino@gmail.com>
2026-04-14 09:14:56 +0200

    Aggiunto tanta roba al carrello.service per farlo funzionare

--- ECommerceFE/src/app/services/carrello.service.ts
+++ ECommerceFE/src/app/services/carrello.service.ts
@@ -1,25 +1,34 @@
-import { inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { GiochiModel } from '../models/giochi-model';
+
+export interface CarrelloItem extends GiochiModel {
+  quantita: number;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarrelloService {
-
-  private carrello: GiochiModel[] = [];
-  private carrelloService = inject(CarrelloService);
+  private carrello: CarrelloItem[] = [];
 
   aggiungi(gioco: GiochiModel) {
-  this.carrelloService.aggiungi(gioco);
-  alert(`Aggiunto al carrello: ${gioco.titolo}`);
-}
+    const esistente = this.carrello.find(item => item.id === gioco.id);
+    if (esistente) {
+      esistente.quantita += 1;
+    } else {
+      this.carrello.push({ ...gioco, quantita: 1 });
+    }
+  }
 
   getCarrello() {
     return this.carrello;
   }
 
   getTotale(): number {
-    return this.carrello.reduce((tot, gioco) => tot + +gioco.prezzo, 0);
+    return this.carrello.reduce(
+      (tot, item) => tot + (parseFloat(item.prezzo) || 0) * item.quantita,
+      0
+    );
   }
 
   rimuovi(index: number) {
