4d6cfa52f6025156ceac58ace913274d7f004d32
simonpirino-lgtm <simonpirino@gmail.com>
2026-04-16 15:30:52 +0200

    ciao

--- ECommerceFE/src/app/services/session-storage.service.ts
+++ ECommerceFE/src/app/services/session-storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { iterator } from 'rxjs/internal/symbol/iterator';
 
 @Injectable({
   providedIn: 'root',
@@ -18,6 +19,7 @@ export class SessionStorageService {
 
   remove(key: string): void {
     sessionStorage.removeItem(key);
+  
   }
 
   clear(): void {
