641296bae945bfa65dac1f6156b39b894d8d5346
andre-a520 <andreafilippocontu5758@gmail.com>
2026-04-29 15:27:56 +0200

    notificheLette

--- Backend/src/routes/notifiche.route.js
+++ Backend/src/routes/notifiche.route.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getNotificheUtente } = require('../controllers/notifiche.controller');
+const { getNotificheUtente, segnaComeLetta, segnaTutteComeLette } = require('../controllers/notifiche.controller');
 const { verifyToken } = require('../services/auth.middleware');
 
 router.get('/', verifyToken, getNotificheUtente);
+
+router.patch('/:id', verifyToken, segnaComeLetta);
+router.patch('/', verifyToken, segnaTutteComeLette);
 
 module.exports = router;
